bc3f056e607948f385f27d3436c2e1668f5f89df
ashtrus <zygimantas.butas@gmail.com>
2023-04-27 16:30:08 +0200

    gradient added

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import {Text, StyleSheet, View} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import SegmentedControl from '@react-native-segmented-control/segmented-control';
+import LinearGradient from 'react-native-linear-gradient';
 import {COLORS} from '../styles';
 
 const HomeScreen = () => {
@@ -27,6 +28,18 @@ const HomeScreen = () => {
           fontStyle={styles.whiteText}
           activeFontStyle={styles.whiteText}
         />
+
+        <LinearGradient
+          colors={['#F988FF', '#3D9AFF']}
+          style={styles.gradient}
+          useAngle
+          angle={98.41}>
+          <View style={styles.row}>
+            <Text style={styles.amount}>2000</Text>
+            <Text style={styles.units}>ml</Text>
+          </View>
+          <Text style={styles.rank}>You are rank 1 today</Text>
+        </LinearGradient>
       </View>
     </SafeAreaView>
   );
@@ -75,6 +88,38 @@ const styles = StyleSheet.create({
   },
   whiteText: {
     color: COLORS.white,
+  },
+  gradient: {
+    marginTop: 37,
+    marginBottom: 34,
+    paddingVertical: 13,
+    borderRadius: 30,
+    alignItems: 'center',
+  },
+  row: {
+    flexDirection: 'row',
+  },
+  amount: {
+    fontFamily: 'Inter-Bold',
+    fontSize: 56,
+    lineHeight: 68,
+    marginRight: 4,
+    color: '#262626',
+  },
+  units: {
+    fontFamily: 'Inter-Bold',
+    fontSize: 20,
+    lineHeight: 24,
+    color: '#262626',
+    opacity: 0.7,
+    alignSelf: 'center',
+  },
+  rank: {
+    fontFamily: 'Inter-Medium',
+    fontSize: 14,
+    lineHeight: 17,
+    color: '#262626',
+    opacity: 0.7,
   },
 });
 
