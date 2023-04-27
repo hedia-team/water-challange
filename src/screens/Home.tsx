10690dc4938ada8ff4ddf84ae7fd083621b619f2
ashtrus <zygimantas.butas@gmail.com>
2023-04-27 17:09:12 +0200

    initial section view

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,9 +1,41 @@
 import React, {useState} from 'react';
-import {Text, StyleSheet, View} from 'react-native';
+import {Text, SectionList, StyleSheet, View} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import SegmentedControl from '@react-native-segmented-control/segmented-control';
 import LinearGradient from 'react-native-linear-gradient';
 import {COLORS} from '../styles';
+import Calendar from '../assets/icons/Calendar';
+
+const DATA = [
+  {
+    title: 'Today February 28th',
+    data: [
+      {time: '12:12', amount: '200'},
+      {time: '13:12', amount: '500'},
+    ],
+  },
+  {
+    title: 'Wednesday February 27th',
+    data: [
+      {time: '12:12', amount: '200'},
+      {time: '13:12', amount: '500'},
+    ],
+  },
+  {
+    title: 'Tuesday February 26th',
+    data: [
+      {time: '12:12', amount: '200'},
+      {time: '13:12', amount: '500'},
+    ],
+  },
+  {
+    title: 'Monday February 25th',
+    data: [
+      {time: '12:12', amount: '200'},
+      {time: '13:12', amount: '500'},
+    ],
+  },
+];
 
 const HomeScreen = () => {
   const [index, setIndex] = useState(0);
@@ -40,6 +72,23 @@ const HomeScreen = () => {
           </View>
           <Text style={styles.rank}>You are rank 1 today</Text>
         </LinearGradient>
+
+        <SectionList
+          sections={DATA}
+          keyExtractor={item => item.time}
+          renderSectionHeader={({section: {title}}) => (
+            <View style={styles.sectionHeaderItem}>
+              <Calendar style={styles.calendarIcon} />
+              <Text style={styles.sectionHeader}>{title}</Text>
+            </View>
+          )}
+          renderItem={({item}) => (
+            <View style={styles.item}>
+              <Text style={styles.time}>{item.time}</Text>
+              <Text style={styles.value}>{item.amount}</Text>
+            </View>
+          )}
+        />
       </View>
     </SafeAreaView>
   );
@@ -120,6 +169,42 @@ const styles = StyleSheet.create({
     lineHeight: 17,
     color: '#262626',
     opacity: 0.7,
+  },
+  sectionHeaderItem: {
+    flexDirection: 'row',
+    borderBottomColor: '#515151',
+    borderBottomWidth: 1,
+    backgroundColor: COLORS.dark,
+  },
+  calendarIcon: {
+    marginLeft: 8,
+  },
+  sectionHeader: {
+    fontFamily: 'Inter-Medium',
+    fontSize: 20,
+    lineHeight: 24,
+    color: '#bebebe',
+    backgroundColor: COLORS.dark,
+    marginLeft: 4,
+    paddingBottom: 14,
+  },
+  item: {
+    flexDirection: 'row',
+    marginVertical: 10,
+    justifyContent: 'space-between',
+    // backgroundColor: COLORS.pink,
+  },
+  time: {
+    fontFamily: 'Inter-Regular',
+    fontSize: 17,
+    lineHeight: 21,
+    color: COLORS.white,
+  },
+  value: {
+    fontFamily: 'Inter-Bold',
+    fontSize: 20,
+    lineHeight: 24,
+    color: COLORS.white,
   },
 });
 
