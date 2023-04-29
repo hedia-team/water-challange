f85a5be03c0c785562d27d1f754754414b9050b4
Cosmin-Madalin Stan <cosmin@hedia.co>
2023-04-29 15:00:32 +0200

    get team drinks

---
+++ src/api/getTeamDrinks.ts
@@ -0,0 +1,19 @@
+import {Drinks} from '../data';
+
+export const getTeamDrinks = async (teamId: string): Promise<Drinks[]> => {
+  const token =
+    'vtUWEwUgJJnz4Aay6w9TcxyUKUXsdJJdK4GdgdAiaGCiLDjqpszB7k3nk3AQAQWF';
+  const url = `https://hydratey.hedia.org/api/v1/${teamId}/drinks}`;
+  const options = {
+    method: 'GET',
+    headers: {
+      Authorization: `token ${token}`,
+    },
+  };
+
+  const response = await fetch(url, options);
+  if (!response.status.toString().startsWith('2')) {
+    return [];
+  }
+  return response.json();
+};
