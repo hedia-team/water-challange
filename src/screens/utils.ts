ce2f0181310ecb18c7db8a35da93d9762298fea3
Cosmin-Madalin Stan <cosmin@hedia.co>
2023-05-01 17:41:17 +0200

    card for total and rank

---
+++ src/screens/utils.ts
@@ -0,0 +1,47 @@
+import {Drink, Team} from '../data';
+interface TotalByDrinker {
+  [drinkerId: string]: number;
+}
+export interface TeamWithTotalAmount {
+  team: Team;
+  totalAmount: number;
+}
+
+export const getSortedDrinkers = (drinks: Array<Drink>) => {
+  const totalByDrinker: TotalByDrinker = drinks?.reduce(
+    (acc: TotalByDrinker, curr) => {
+      const {drinkerId, amount} = curr;
+      acc[drinkerId] = acc[drinkerId] ? acc[drinkerId] + amount : amount;
+      return acc;
+    },
+    {},
+  );
+
+  const sortedDrinkersWithAmount = Object.entries(totalByDrinker)
+    .sort(([, a], [, b]) => b - a)
+    .map(([drinkerId, total]) => ({drinkerId, total}));
+  return sortedDrinkersWithAmount;
+};
+
+export const getRanking = (drinks: Array<Drink>, drinkerId: string) => {
+  const sortedDrinkersWithAmount = getSortedDrinkers(drinks);
+  return (
+    sortedDrinkersWithAmount.findIndex(({drinkerId: id}) => id === drinkerId) +
+    1
+  );
+};
+
+export const sortTeamsByTotalAmount = (
+  teams: Team[],
+  drinks: Drink[],
+): TeamWithTotalAmount[] => {
+  const teamsWithTotalAmount =
+    teams?.map(team => ({
+      team,
+      totalAmount: drinks
+        ?.filter(d => d.teamId === team.id)
+        .reduce((total, d) => total + d.amount, 0),
+    })) ?? [];
+
+  return teamsWithTotalAmount.sort((a, b) => b!.totalAmount - a!.totalAmount);
+};
